d6e937e3387a4e939e8b4c1f28693aba9f410fa8
William <111227194+ViliamOleos@users.noreply.github.com>
2025-01-03 02:20:21 +0500

    main.cpp*>@(supress press)
    we goin' dictatorial with this one

--- main.cpp
+++ main.cpp
@@ -3,22 +3,26 @@
 
 using namespace std;
 
-LRESULT CALLBACK TST_BTNhookproc(int ncode, WPARAM wParam, LPARAM lParam) {
-    if (wParam == WM_KEYDOWN && lParam != NULL)
-    {
-        std::cout<<((LPKBDLLHOOKSTRUCT)lParam)->vkCode<<' '; //74
+LRESULT CALLBACK TST_BTNhookproc(int ncode, WPARAM wParam, LPARAM lParam)
+{
+    if(wParam == WM_KEYDOWN && lParam != NULL) { // keydown could be problematic, ill change it later
+        std::cout<<((LPKBDLLHOOKSTRUCT)lParam)->vkCode<<' '; // T = 84
+
+        if(((LPKBDLLHOOKSTRUCT)lParam)->vkCode==84) { // if key = T
+            return true; // huh wdym never heard of a T being pressed
+        }
     }
 
-    return CallNextHookEx(NULL, ncode, wParam, lParam);
+    return CallNextHookEx(NULL, ncode, wParam, lParam); // pass the torch
 }
 
 int main()
 {
     HHOOK TST_BTN = SetWindowsHookExW(WH_KEYBOARD_LL, TST_BTNhookproc,  NULL, 0);
 
-    MessageBoxW(NULL, L"hooking", L"", MB_ICONEXCLAMATION | MB_SYSTEMMODAL);
+    MessageBoxW(NULL, L"hooking", L"", MB_ICONEXCLAMATION | MB_SYSTEMMODAL); // Dialogue Box duh
 
-    UnhookWindowsHookEx(TST_BTN);
+    UnhookWindowsHookEx(TST_BTN); // baye
 
     return 0;
 }
