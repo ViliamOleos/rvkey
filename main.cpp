32b75f309d8258f243ee5a67930909d7b84a074c
William <111227194+ViliamOleos@users.noreply.github.com>
2025-01-30 12:36:57 +0500

    main.cpp*>@(one less if statement)
    :D

--- main.cpp
+++ main.cpp
@@ -8,9 +8,7 @@ LRESULT CALLBACK TST_BTNhookproc(int ncode, WPARAM wParam, LPARAM lParam)
     if(wParam == WM_KEYDOWN && lParam != NULL) { // keydown could be problematic, ill change it later
         std::cout<<((LPKBDLLHOOKSTRUCT)lParam)->vkCode<<' '; // T = 84
 
-        if(((LPKBDLLHOOKSTRUCT)lParam)->vkCode==84) { // if key = T
-            return true; // huh wdym never heard of a T being pressed
-        }
+        return ((LPKBDLLHOOKSTRUCT)lParam)->vkCode==84; // if key = T then never heard of a T being pressed end
     }
 
     return CallNextHookEx(NULL, ncode, wParam, lParam); // pass the torch
