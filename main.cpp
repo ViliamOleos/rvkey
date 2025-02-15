f3134217a6f2168a982277d15b9e7aa051cfd05b
William <111227194+ViliamOleos@users.noreply.github.com>
2025-02-15 16:53:43 +0500

    main.cpp*>@(replace button memory)
    it actually compiles now guys yaaay lol

--- main.cpp
+++ main.cpp
@@ -2,16 +2,22 @@
 #include <iostream>
 #include <cstdio>
 
+DWORD replacer = 0; // initializing this var so it doesn't carry rubbish
+
 LRESULT CALLBACK TST_BTNhookproc(int ncode, WPARAM wParam, LPARAM lParam)
 {
     if(wParam == WM_KEYDOWN && lParam != NULL) { // keydown could be problematic, ill change it later
         std::cout<<((LPKBDLLHOOKSTRUCT)lParam)->vkCode<<' '; // print pressed keys
 
+        if(!replacer){
+            replacer = ((LPKBDLLHOOKSTRUCT)lParam)->vkCode;
+        }
+
         switch(((LPKBDLLHOOKSTRUCT)lParam)->vkCode) { // switch statement because this one fits nicely
             case 27: // case escape key pressed then
                 PostQuitMessage(0); // quit program
             case 81: // the q key
-                SendInput(/*finish this*/); // it should simulate the key "replacer"
+                std::cout << replacer;//SendInput(/*finish this*/); // it should simulate the key "replacer"
         }
 
         //return ((LPKBDLLHOOKSTRUCT)lParam)->vkCode==84; // if key = T then never heard of a T being pressed end
@@ -22,10 +28,9 @@ LRESULT CALLBACK TST_BTNhookproc(int ncode, WPARAM wParam, LPARAM lParam)
 
 int main()
 {
-    HHOOK TST_BTN = SetWindowsHookExW(WH_KEYBOARD_LL, TST_BTNhookproc,  NULL, 0);
-    MSG mesag; char replacer;
-
-    replacer = char(getchar());
+    HHOOK TST_BTN = SetWindowsHookExW(WH_KEYBOARD_LL, TST_BTNhookproc,  NULL, 0); // h o o k e n g
+    // imagine DWORD replacer here if that makes it easier
+    MSG mesag; // carrier of the button master
 
     while (GetMessage(&mesag, NULL, 0, 0)) {
         TranslateMessage(&mesag);
